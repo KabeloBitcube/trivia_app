6d47c3bd03e8ee5606296510e585c9fd1939669c
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-05-23 11:36:46 +0200

    V3 - Added question mark images to home page

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -6,8 +6,30 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
      return Scaffold(
-      body: Center(
-        child: const Text('Home Page'),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.deepPurpleAccent,
+        child: Column(
+          children: [
+            SizedBox(height: 20,),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.4,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset('assets/images/Object8.png', width: 100,),
+                      Image.asset('assets/images/Object10.png'),
+                      Image.asset('assets/images/Object9.png', width: 100,)
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
      );
   }
