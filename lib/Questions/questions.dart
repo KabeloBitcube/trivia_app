c2b44bd22fc2bc4ccc7278f39da6b4f6eb25a967
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-05-26 08:45:42 +0200

    V9 - API integration > fetchQuiz function, http and html unescape packages added

--- lib/Questions/questions.dart
+++ lib/Questions/questions.dart
@@ -1,14 +1,92 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:html_unescape/html_unescape_small.dart';
 import 'package:trivia_app/Components/quizbutton.dart';
 import 'package:trivia_app/Components/quizcontainer.dart';
+import 'package:http/http.dart' as http;
 
-class Questions extends StatelessWidget {
+class Questions extends StatefulWidget {
   Questions({super.key});
 
+  @override
+  State<Questions> createState() => _QuestionsState();
+}
+
+class _QuestionsState extends State<Questions> {
+  //For testing layout
   final String quiz =
       "When Gmail first launched, how much storage did it provide for your email?";
 
   final List<String> options = ['512GB', '10GB', '5GB', 'Unlimited'];
+
+  //API integration
+  List<Map<String, dynamic>> questions = [];
+  int currentIndex = 0;
+  int score = 0;
+  String? selectedAnswer;
+  bool showNext = false;
+  final unescape = HtmlUnescape();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchQuiz();
+  }
+
+  Future<void> fetchQuiz() async {
+    final response = await http.get(
+      Uri.parse(
+        'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy',
+      ),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    log('Response status: ${response.statusCode}');
+    log('Response body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      if (data['results'] != null && data['results'].isNotEmpty) {
+        List<Map<String, dynamic>> loadedQuestions = [];
+
+        for (var item in data['results']) {
+          String questionText = unescape.convert(item['question']);
+          String correctAnswer = unescape.convert(item['correct_answer']);
+          List<String> options = List<String>.from(
+            item['incorrect_answers'].map(
+              (e) => unescape.convert(e.toString()),
+            ),
+          );
+
+          options.add(correctAnswer);
+          options.shuffle();
+
+          loadedQuestions.add({
+            'question' : questionText,
+            'correct_answer' : correctAnswer,
+            'options' : options
+          });
+
+          if(!mounted) return;
+
+          setState(() {
+            questions = loadedQuestions;
+            currentIndex = 0;
+            score = 0;
+            selectedAnswer = null;
+            showNext = false;
+          });
+        }
+
+      }
+    }
+    else{
+      log('Failed to load API. Response status: ${response.statusCode}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +99,13 @@ class Questions extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Question 6', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+            const Text(
+              'Question 6',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             SizedBox(height: 50),
             Stack(
               children: [
@@ -59,13 +143,13 @@ class Questions extends StatelessWidget {
             //   },
             // )
             QuizButton(option: options[0]),
-            SizedBox(height: 10,),
+            SizedBox(height: 10),
             QuizButton(option: options[1]),
-            SizedBox(height: 10,),
+            SizedBox(height: 10),
             QuizButton(option: options[2]),
-            SizedBox(height: 10,),
+            SizedBox(height: 10),
             QuizButton(option: options[3]),
-            SizedBox(height: 10,),
+            SizedBox(height: 10),
           ],
         ),
       ),
